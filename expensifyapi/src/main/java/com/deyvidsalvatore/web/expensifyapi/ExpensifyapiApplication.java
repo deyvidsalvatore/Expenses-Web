d382329c196a601c27618afecfc4a6081f2ebfa9
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-07-21 16:20:43 -0300

    initial commit

---
+++ expensifyapi/src/main/java/com/deyvidsalvatore/web/expensifyapi/ExpensifyapiApplication.java
@@ -0,0 +1,13 @@
+package com.deyvidsalvatore.web.expensifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensifyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensifyapiApplication.class, args);
+	}
+
+}
